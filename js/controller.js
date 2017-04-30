3ddccf38b7e8708de0ade629a501f7157aa0fef2
Pankamol Srikaew <aim.ske09@gmail.com>
2017-04-30 14:24:32 +0900

    summary template

--- js/controller.js
+++ js/controller.js
@@ -1,49 +1,90 @@
 angular.module('myApp')
-.controller('tableCtrl', function($scope, $http){
-	$http({
-		method: 'GET',
-		url: 'http://192.168.0.2:8082/users'
-	}).then(function(resp){
-		console.log('success!')
-		console.log(resp)
-		$scope.peopleList = resp.data;
 
-	}).then(function(resp){
-	})
-
-
-})
 
 .controller('myCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.showMoreInfo = function(){
+		return false;
+	}
+	$scope.data = {
+		0 : {
+			"room" : "101",
+			"renter" : "xxx",
+			"info" : [
+				{ "water" : [
+						{"previous" : "100" },
+						{"now" : "100" },
+						{"total" : "0" }
+					] 
+				},
+				{ "ele" : [
+						{"previous" : "200" },
+						{"now" : "100" },
+						{"total" : "100" }
+					] 
+				}
+			]
+		},1 : {
+			"room" : "102",
+			"renter" : "yyy",
+			"info" : [
+				{ "water" : [
+						{"previous" : "100" },
+						{"now" : "100" },
+						{"total" : "0" }
+					] 
+				},
+				{ "ele" : [
+						{"previous" : "200" },
+						{"now" : "100" },
+						{"total" : "100" }
+					] 
+				}
+			]
+		},2 : {
+			"room" : "103",
+			"renter" : "zzz",
+			"info" : [
+				{ "water" : [
+						{"previous" : "100" },
+						{"now" : "100" },
+						{"total" : "0" }
+					] 
+				},
+				{ "ele" : [
+						{"previous" : "200" },
+						{"now" : "100" },
+						{"total" : "100" }
+					] 
+				}
+			]
+		},3 : {
+			"room" : "104",
+			"renter" : "aaa",
+			"info" : [
+				{ "water" : [
+						{"previous" : "100" },
+						{"now" : "100" },
+						{"total" : "0" }
+					] 
+				},
+				{ "ele" : [
+						{"previous" : "200" },
+						{"now" : "100" },
+						{"total" : "100" }
+					] 
+				}
+			]
+		}
 
-	$http({
-  method: 'GET',
-  url: 'http://localhost:8081/getDocuments'
-}).then(function successCallback(response) {
-    console.log(response)
-  }, function errorCallback(response) {
-    console.log('err')
-  });
-
-
-}])
-
-
-.controller('addNewUser', function($scope, $http){
-	$scope.user = {};
-	$scope.addUser = function(){
-		$http({
-			method: 'POST',
-			url: 'http://192.168.0.2:8082/newOfficer',
-			data: $scope.user
-		}).then(function(resp){
-			console.log(resp)
-		}).then(function(resp){
-			console.log('fail!!!!!')
-			console.log(resp)
-
-		})
+	}
+	$scope.rentInfo = function(index){
+		var roomData = $scope.data[index];
+		$scope.selectedRoom = roomData.room;
+		console.log($scope.data[index]);
+		$scope.showMoreInfo = function(){
+			return true;
+		}
 	}
 
 
-})
+}])
